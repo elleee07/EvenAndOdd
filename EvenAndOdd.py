0d513668b7073535b5b17af985a00f811fa722a4
Ella Marie A. Mallari <elleee.mallari@gmail.com>
2023-04-26 22:00:53 +0800

    Output

--- EvenAndOdd.py
+++ EvenAndOdd.py
@@ -2,10 +2,9 @@
 # BSCPE 1-4
 
 # EVEN AND ODD
+
 # open numbers.txt (read), even.txt (append) and odd.txt (append)
 with open("numbers.txt") as input_file, open("even.txt", "a") as output_even, open("odd.txt", "a") as output_odd:
-    output_even.write("EVEN NUMBERS \n")
-    output_odd.write("ODD NUMBERS \n")
     # read it line by line 
     for line in input_file:
         input_num = int(line)
