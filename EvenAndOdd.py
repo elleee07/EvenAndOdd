5d78bb80e00eef8570abfceda1c78d055382ba17
Ella Marie A. Mallari <elleee.mallari@gmail.com>
2023-04-26 20:30:35 +0800

    Check if numbers are odd or even

--- EvenAndOdd.py
+++ EvenAndOdd.py
@@ -6,5 +6,8 @@
 with open("numbers.txt") as input_file, open("even.txt", "a") as output_even, open("odd.txt", "a") as output_odd: 
     for line in input_file:
         print(line.strip())
+        input_num = int(line)
     # if even 
+    if input_num % 2 == 0:
     # if odd 
+    else: 
