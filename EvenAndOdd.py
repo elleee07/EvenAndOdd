50fb471a0a5c3fb84115e83d898d39966a87e093
Ella Marie A. Mallari <elleee.mallari@gmail.com>
2023-04-26 20:13:30 +0800

    Create txt.file

--- EvenAndOdd.py
+++ EvenAndOdd.py
@@ -1,2 +1,9 @@
 # ELLA MARIE A. MALLARI
 # BSCPE 1-4
+
+# EVEN AND ODD
+# open numbers.txt (read), even.txt (append) and odd.txt (append)
+with open("numbers.txt") as input_file, open("even.txt", "a") as output_even, open("odd.txt", "a") as output_odd: 
+    for line in input_file:
+        print(line.strip())
+    # if even 
